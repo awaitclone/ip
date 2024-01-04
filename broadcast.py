575f49019387ed0f2a7393b131d37e28cec41c9a
usermlc <75613235+usermlc@users.noreply.github.com>
2024-01-04 20:02:25 +0200

    Update broadcast.py

--- broadcast.py
+++ broadcast.py
@@ -17,10 +17,25 @@ class MyBroadcasterMod(loader.Module):
 
     async def send_message(self):
         try:
-            await self.client.send_file('zelenka_services', 'zelenka_banner.png', caption=message_text)
-            await self.client.send_message('Тут канал куда будут ошибки', self.strings("success"))
+            image_url = 'https://i.imgur.com/iOYNbtV.png'  # URL изображения
+            message_text = (
+                "Тут текст\n\n"
+                "ЗАЕБАЛСЯ ИСКАТЬ НЕ ДОРОГОЙ НО КАЧЕСТВЕННЫЙ ДИЗАЙН ❓\n\n"
+                "Рады вам представить — MORIA DESIGN, лучший дизайн за лучшие цены во всем рынке,\n"
+                "средний чек — 250-500₽\n\n"
+                "Делаем:\n"
+                "⚙️- Аватарки\n"
+                "👍- Рекламные баннеры\n"  
+                "👌- Оформление тем\n"
+                "🌐- Анимированные работы\n\n"
+                "Портфолио: @moriadesign\n"
+                "(https://t.me/+wn_45A9wkbViMmFi)Связь: @imfckngmoriarty\n"
+                "Гарант: @zelenka_guarantor_robot"
+            )
+            await self.client.send_file('zelenka_services', image_url, caption=message_text)
+            await self.client.send_message('kgfdgkdfgk324', self.strings("success"))
         except Exception as e:
-            await self.client.send_message('Тут канал куда будут ошибки', self.strings("error").format(str(e)))
+            await self.client.send_message('kgfdgkdfgk324', self.strings("error").format(str(e)))
 
     @loader.command(ru_doc="Запустить рассылку сообщений")
     async def mybroadcastcmd(self, message: Message):
@@ -34,10 +49,6 @@ class MyBroadcasterMod(loader.Module):
                     await self.send_message()
                 await asyncio.sleep(max(0, interval - (current_time - last_sent_time)))
             except Exception as e:
-                await self.client.send_message('Тут канал куда будут ошибки', self.strings("error").format(str(e)))
+                await self.client.send_message('kgfdgkdfgk324', self.strings("error").format(str(e)))
                 last_sent_time = time.time()
                 await asyncio.sleep(max(0, interval - (current_time - last_sent_time)))
-
-message_text = (
-    "Тут текст"
-)
